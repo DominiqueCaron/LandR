dc2ed2078d332e2730c38f18dee28faf9aa2dfc2
Alex Chubaty <achubaty@for-cast.ca>
2024-12-23 10:24:35 -0700

    more cleanup with prev

--- tests/testthat/test-compareRas.R
+++ tests/testthat/test-compareRas.R
@@ -41,8 +41,6 @@ test_that("test .compareRas, .compareCRS -- rasters only", {
 
 test_that("test .compareRas, .compareCRS -- vectors only", {
   require(reproducible)
-  require(raster)
-  require(sf)
 
   opts <- options(
     reproducible.inputPaths = NULL,
@@ -54,14 +52,14 @@ test_that("test .compareRas, .compareCRS -- vectors only", {
   on.exit(options(opts), add = TRUE)
 
   f <- system.file("ex/lux.shp", package = "terra")
-  v <- vect(f)
-  v2 <- project(v, crs(v))
+  v <- terra::vect(f)
+  v2 <- terra::project(v, crs(v))
 
   expect_true(.compareRas(v, v))
   expect_true(.compareRas(v, v, v))
   expect_true(.compareRas(v, v, v2))
 
-  v2 <- project(v2, "EPSG:2169")
+  v2 <- terra::project(v2, "EPSG:2169")
   expect_error(.compareRas(v, v2))
   expect_false(.compareRas(v, v2, stopOnError = FALSE))
 
@@ -82,8 +80,8 @@ test_that("test .compareRas, .compareCRS -- vectors only", {
   expect_true(.compareRas(v3, v5))
 
   ## with sf
-  v6 <- st_read(f)
-  v7 <- st_as_sf(v5)
+  v6 <- sf::st_read(f)
+  v7 <- sf::st_as_sf(v5)
   expect_true(.compareRas(v, v4, v6))
   expect_error(.compareRas(v6, v7))
   expect_false(.compareRas(v6, v7, stopOnError = FALSE))
@@ -97,8 +95,6 @@ test_that("test .compareRas, .compareCRS -- vectors only", {
 
 test_that("test .compareRas, .compareCRS -- vectors and rasters", {
   require(reproducible)
-  require(raster)
-  require(sf)
 
   opts <- options(
     reproducible.inputPaths = NULL,
@@ -110,22 +106,22 @@ test_that("test .compareRas, .compareCRS -- vectors and rasters", {
   on.exit(options(opts), add = TRUE)
 
   f <- system.file("ex/lux.shp", package = "terra")
-  v <- vect(f)
+  v <- terra::vect(f)
 
-  ras <- rast(v, resolution = 0.1)
+  ras <- terra::rast(v, resolution = 0.1)
 
   expect_true(.compareRas(v, ras, ext = FALSE))
   expect_error(.compareRas(v, ras))
   expect_false(.compareRas(v, ras, stopOnError = FALSE))
 
-  v2 <- project(v, "EPSG:2169")
+  v2 <- terra::project(v, "EPSG:2169")
   expect_error(.compareRas(ras, v2, ext = FALSE))
   expect_false(.compareRas(ras, v2, ext = FALSE, stopOnError = FALSE))
   expect_true(.compareRas(ras, v2, crs = FALSE, ext = FALSE))
 
   ## with raster and SPDF
-  ras2 <- raster(ras)
-  v3 <- shapefile(f)
+  ras2 <- raster::raster(ras)
+  v3 <- raster::shapefile(f)
   expect_true(.compareRas(v, v3, ras, ras2, ext = FALSE))
   expect_error(.compareRas(v, v3, ras, ras2))
   expect_false(.compareRas(v, v3, ras, ras2, stopOnError = FALSE))
