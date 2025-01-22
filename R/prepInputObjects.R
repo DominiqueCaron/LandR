b22284a5215ed2f21b7f319807d7e8c767da4c3c
Alex Chubaty <achubaty@for-cast.ca>
2025-01-22 11:29:57 -0700

    post-merge cleanup: don't importFrom suggested packages

--- R/prepInputObjects.R
+++ R/prepInputObjects.R
@@ -533,7 +533,6 @@ prepInputsStandAgeMap <- function(..., ageURL = NULL,
 #' @template studyAreaName
 #'
 #' @template cacheTags
-#' @importFrom httr2 request
 #'
 #' @param ... arguments passed to [reproducible::prepInputs()] and [reproducible::Cache()]. If the following arguments
 #'   are not provided, the following values will be used:
@@ -553,8 +552,9 @@ prepInputsStandAgeMap <- function(..., ageURL = NULL,
 #'
 #' @export
 prepRawBiomassMap <- function(studyAreaName, cacheTags, ...) {
-  if (requireNamespace("httr2"))
+  if (requireNamespace("httr2")) {
     httr2::request
+  }
   Args <- list(...)
   if (is.null(Args$url)) {
     Args$url <- paste0(
